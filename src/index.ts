325a9c76c7ff0113c59892bd59c536c42947b4d5
fayismahmood <fmthottathil@gmail.com>
2025-12-02 09:21:49 +0530

    feat: Update environment variables and add HTTP request examples for exams and results

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = new Elysia()
   .get("/", () => "Hello Elysia")
   .use(examRoutes)
   .use(resultRoutes)
-  .listen(3000);
+// .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
